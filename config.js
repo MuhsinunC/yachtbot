eaa3c6b3e40b333103cb20ea509f2df5a35f87a9
Syed Aman <sna274@nyu.edu>
2017-12-17 19:19:38 -0600

    create config file

---
+++ config.js
@@ -0,0 +1,6 @@
+const config = {
+  webhookURL:
+    'https://discordapp.com/api/webhooks/391405913409454082/RBSwOtImrIzvpY2Qf90Pdge2eaHR3EjnDCqjCaZDhi2ACfEsdNwZDb83LLZL9u-_oyv9',
+};
+
+export default config;
