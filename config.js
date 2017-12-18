ccba4aa3a8069f17dd528e21403292f3c42745dc
Syed Aman <sna274@nyu.edu>
2017-12-17 19:58:08 -0600

    update webhook to not include https://

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 const config = {
-  webhookURL:
-    'https://discordapp.com/api/webhooks/391405913409454082/RBSwOtImrIzvpY2Qf90Pdge2eaHR3EjnDCqjCaZDhi2ACfEsdNwZDb83LLZL9u-_oyv9',
+  webhooks: {
+    cryptopricing:
+      '/api/webhooks/391405913409454082/RBSwOtImrIzvpY2Qf90Pdge2eaHR3EjnDCqjCaZDhi2ACfEsdNwZDb83LLZL9u-_oyv9',
+  },
 };
 
 module.exports = config;
