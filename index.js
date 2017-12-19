da8ab18706afe61500227a71dea5f6a942e0c2ff
Syed Aman <sna274@nyu.edu>
2017-12-18 19:02:41 -0600

    Add linting & eslint support (#10)
    * add a linting config
    
    * use .eslintrc format instead of .eslintrc.json
    
    * prevent linting of node_modules
    
    * install all plugins needed to properly lint everything using standard
    
    * fix linting errors
    
    * update yarn lock
    
    * remove comma dangle rule
    
    * fix linting
    
    * fix issue where all JS files weren't being linted

--- index.js
+++ index.js
@@ -4,35 +4,34 @@
  * Application entry point.
  */
 
-const https = require('https');
-const cc = require('cryptocompare');
-const Discord = require('discord.js');
-const client = new Discord.Client();
-global.fetch = require('node-fetch');
+const cc = require('cryptocompare')
+const Discord = require('discord.js')
+const client = new Discord.Client()
+global.fetch = require('node-fetch')
 
-const config = require('./config');
+const config = require('./config')
 
 // indicate bot is connected
 client.on('ready', () => {
-  console.log('Bot is ready!');
-});
+  console.log('Bot is ready!')
+})
 
 // parse user messages and respond accordingly
 client.on('message', message => {
-  const content = message.content;
-  const tickerRegex = /^[$A-Z]{4}$/gi;
+  const content = message.content
+  const tickerRegex = /^[$A-Z]{4}$/gi
 
   if (content === 'fuck') {
-    message.reply('fuck me daddy');
+    message.reply('fuck me daddy')
   } else if (content.match(tickerRegex)) {
-    const response = getMarketValue(content.replace('$', ''));
+    const response = getMarketValue(content.replace('$', ''))
 
     response.then(result => {
-      const reply = `${content}: ${result}`;
-      message.reply(reply);
-    });
+      const reply = `${content}: ${result}`
+      message.reply(reply)
+    })
   }
-});
+})
 
 /**
  * Gets the current market value for any currency.
@@ -40,19 +39,19 @@ client.on('message', message => {
  * @param  {String} ticker - The cryptocurrency symbol i.e $ETH.
  * @return {Number} - The price of the ticker in USD.
  */
-async function getMarketValue(ticker) {
+async function getMarketValue (ticker) {
   try {
     // fetch crypto pricings
-    const VAL = await cc.price(ticker, ['USD']);
-    return VAL.USD;
+    const VAL = await cc.price(ticker, ['USD'])
+    return VAL.USD
   } catch (error) {
-    throw new Error(error);
+    throw new Error(error)
   }
 }
 
 // make the bot login to the server
 try {
-  client.login(config.secret);
+  client.login(config.secret)
 } catch (error) {
-  throw new Error('bot failed to login', error);
+  throw new Error('bot failed to login', error)
 }
