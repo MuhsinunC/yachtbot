ca7068dc224056e023b38f09abec393b1822945f
Syed Aman <saman@elevate.com>
2017-12-20 13:47:06 -0600

    refactor getting the market value of a ticker

--- src/index.js
+++ src/index.js
@@ -88,11 +88,8 @@ client.on('message', async message => {
  */
 async function getMarketValue (ticker) {
   try {
-    // remove the $ from the ticker
-    if (ticker.includes('$')) ticker = ticker.replace('$', '')
-
     // fetch crypto pricings
-    const VAL = await cc.price(ticker.toUpperCase(), ['USD'])
+    const VAL = await cc.price(ticker.replace('$', '').toUpperCase(), ['USD'])
     return VAL.USD
   } catch (error) {
     console.error(error)
