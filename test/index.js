7ee8b5a401ad28f86662d29729325fd69689cfcd
Syed Aman <sna274@nyu.edu>
2017-12-20 21:16:29 -0600

    create sample test file

---
+++ test/index.js
@@ -0,0 +1,9 @@
+const assert = require('assert')
+const describe = require('mocha').describe
+const it = require('mocha').it
+
+describe('Discord crypto bot', () => {
+  it("should respond with 'fuck me, daddy' when user says 'fuck'", () => {
+    assert.equal(1, 1)
+  })
+})
