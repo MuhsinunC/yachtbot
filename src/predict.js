62bb8e4b7e2e1206779367b1a89b8371ed12d70a
Gabriel Garrett <ggaabe@gmail.com>
2018-01-11 21:10:06 -0600

    Rounded predictor

--- src/predict.js
+++ src/predict.js
@@ -7,7 +7,17 @@
 function getPredict(price){
   price = parseFloat(price);
   var priceVariance = price / 9;
-  return price +  Math.floor(Math.random() * priceVariance) - (priceVariance / 2);
+  return roundToXDigits(price +  Math.floor(Math.random() * priceVariance) - (priceVariance / 2));
+}
+
+function roundToXDigits(value, digits) {
+    if(!digits){
+        digits = 2;
+    }
+    value = value * Math.pow(10, digits);
+    value = Math.round(value);
+    value = value / Math.pow(10, digits);
+    return value;
 }
 
 module.exports.predict = {
